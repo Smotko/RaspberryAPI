268f985bbc886f230d036f59820af74ffee8d164
Anže Pečar <anze@pecar.me>
2021-04-29 01:02:51 +0000

    Add tests

---
+++ test_main.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_main():
+    response = client.get("/", allow_redirects=False)
+    assert response.status_code == 307
+    assert response.headers["Location"] == "/docs"
+
+
+def test_uptime():
+    response = client.get("/api/v1/uptime")
+    assert response.status_code == 200
+    assert "up" in response.json()["result"]
+
+
+def test_speach():
+    response = client.post("/api/v1/speach", json={"text": "Hello test"})
+    assert response.status_code == 200, response.json()
+    assert response.json()["result"] == "I have spoken"
